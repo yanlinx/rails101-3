bda1ff64ae90b0977399b99c9a3b7193ae0ff85d
xieyanlin <xieyanlin820@gmail.com>
2017-01-01 10:30:29 +0800

    validation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,33 +1,49 @@
 class GroupsController < ApplicationController
-  def index
-    @groups = Group.all
+
+    def index
+        @groups = Group.all
+  end
+
+    def new
+        @group = Group.new
+  end
+
+    def show
+        @group = Group.find(params[:id])
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def create
+        @group = Group.new(group_params)
+      if @group.save
+        redirect_to groups_path
+      else
+        render :new
+      end
+  end
+
+    def update
+        @group = Group.find(params[:id])
+      if@group.update(group_params)
+        redirect_to groups_path, notice: 'Update success'
+      else
+      render :new
+    end
+
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        redirect_to groups_path, alert: 'Group delete'
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:title, :description)
+  end
 end
-  def new
-    @group = Group.new
-end
-  def show
-    @group = Group.find(params[:id])
-  end
-  def edit
-    @group = Group.find(params[:id])
-  end
-  def create
-    @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
-end
-  def update
-    @group = Group.find(params[:id])
-    @group.update(group_params)
-    redirect_to groups_path,notice: 'Update success'
-  end
-  def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    redirect_to groups_path,alert: 'Group delete'
-  end
-   private
-   def group_params
-   params.require(:group).permit(:title, :description)
- end
- end
