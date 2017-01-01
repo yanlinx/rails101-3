d8be07d39ad428d3283df5e12fe571419120b964
xieyanlin <xieyanlin820@gmail.com>
2017-01-01 23:28:20 +0800

    群组成员和非群组成员

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,6 @@ class Group < ApplicationRecord
   validates :title,presence: true
 
   has_many :group_relationships
-  has_many :members, through: :group_relationships,source: :user
+  has_many :members, through: :group_relationships, source: :user
 
 end
